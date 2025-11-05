648000e902e14823acfc1ce7a3ce26bb10e69a05
senkoslav <yaroslav76071@mail.ru>
2025-11-05 17:50:19 +0300

    types

--- src/auth/session.serializer.ts
+++ src/auth/session.serializer.ts
@@ -3,11 +3,11 @@ import { PassportSerializer } from '@nestjs/passport';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  serializeUser(user: any, done: (err: Error, user: any) => void): any {
+  serializeUser(user: any, done: (err: Error | null, user: any) => void): any {
     done(null, user);
   }
 
-  deserializeUser(payload: any, done: (err: Error, payload: string) => void): any {
+  deserializeUser(payload: any, done: (err: Error | null, payload: any) => void): any {
     done(null, payload);
   }
 }
